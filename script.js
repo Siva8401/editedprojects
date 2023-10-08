f73813207a578e8aa58c9acb46dac2bbce0f3ce6
Sivachandran <141358439+Siva8401@users.noreply.github.com>
2023-10-09 01:51:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const videoPlayer = document.getElementById('video-player');
+const playPauseButton = document.getElementById('play-pause-button');
 const rotateButton = document.getElementById('rotate-button');
+let isPlaying = false;
 let isRotated = false;
+
+playPauseButton.addEventListener('click', function () {
+    if (isPlaying) {
+        videoPlayer.pause();
+        playPauseButton.innerText = 'Play';
+    } else {
+        videoPlayer.play();
+        playPauseButton.innerText = 'Pause';
+    }
+    isPlaying = !isPlaying;
+});
 
 rotateButton.addEventListener('click', function () {
     if (isRotated) {

5ff215a4f519e9b09ba26a31ab706d4ae8698384
Sivachandran <141358439+Siva8401@users.noreply.github.com>
2023-10-09 02:41:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const videoPlayer = document.getElementById('video-player');
+const rotationInput = document.getElementById('rotation');
+const rotationValue = document.getElementById('rotation-value');
+
+rotationInput.addEventListener('input', function () {
+    const rotationDegree = this.value;
+    videoPlayer.style.transform = `rotate(${rotationDegree}deg)`;
+    rotationValue.innerText = `${rotationDegree} degrees`;
+});
 
